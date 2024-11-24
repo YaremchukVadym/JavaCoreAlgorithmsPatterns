38eb4bbf4020cbaceb528bbcb13256cae38ed42d
YaremchukVadym <yaremchuk2003vadym@gmail.com>
2024-11-24 23:50:22 +0200

    Dijkstra

--- src/AdvancedJava/AnotherTopic/Dijlkstra/Dijkstra.java
+++ src/AdvancedJava/AnotherTopic/Dijlkstra/Dijkstra.java
@@ -1,5 +1,16 @@
 package AdvancedJava.AnotherTopic.Dijlkstra;
 
+import java.util.PriorityQueue;
+
 public class Dijkstra {
 
+    PriorityQueue<Vertex> queue;
+
+    public Dijkstra() {
+        queue = new PriorityQueue<>();
+    }
+
+    public void compute(Vertex vertex) {
+
+    }
 }
--- src/AdvancedJava/AnotherTopic/Dijlkstra/Vertex.java
+++ src/AdvancedJava/AnotherTopic/Dijlkstra/Vertex.java
@@ -3,14 +3,16 @@ package AdvancedJava.AnotherTopic.Dijlkstra;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Vertex {
+public class Vertex implements Comparable<Vertex> {
     String name;
     boolean visited;
     List<Edge> neighbours;
+    double distance;
 
     public Vertex(String name){
         this.name = name;
         neighbours = new ArrayList<>();
+        distance = Double.MAX_VALUE;
     }
 
     public void addNeighbour(Edge e){
@@ -33,4 +35,16 @@ public class Vertex {
         return neighbours;
     }
 
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    @Override
+    public int compareTo(Vertex o) {
+        return Double.compare(this.distance, o.distance);
+    }
 }
