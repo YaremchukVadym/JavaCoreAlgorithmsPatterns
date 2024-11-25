f202c590c2e8b5f9c63f624a54d8e5d3b4ffa75c
YaremchukVadym <yaremchuk2003vadym@gmail.com>
2024-11-25 12:56:00 +0200

    NestedClasses

---
+++ src/AdvancedJava/NestedClasses/Test.java
@@ -0,0 +1,9 @@
+package AdvancedJava.NestedClasses;
+
+public class Test {
+    static class A{}
+    class B{}
+    void method(){
+        class C{}
+    }
+}
---
+++ src/AdvancedJava/NestedClasses/static_nested_class/Car.java
@@ -0,0 +1,46 @@
+package AdvancedJava.NestedClasses.static_nested_class;
+
+public class Car {
+    String color;
+    int doorCount;
+    Engine engine;
+
+    public Car(String color, int doorCount, Engine engine) {
+        this.color = color;
+        this.doorCount = doorCount;
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "My car: {" +
+                "color='" + color + '\'' +
+                ", doorCount=" + doorCount +
+                ", engine=" + engine +
+                '}';
+    }
+
+    public static class Engine {
+        int horsePower;
+
+        public Engine(int horsePower) {
+            this.horsePower = horsePower;
+        }
+
+        @Override
+        public String toString() {
+            return "My engine: {" +
+                    "horsePower=" + horsePower +
+                    '}';
+        }
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        Car.Engine engine = new Car.Engine(10);
+        System.out.println(engine);
+        Car car = new Car("red", 10, engine);
+        System.out.println(car);
+    }
+}
